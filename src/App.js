8ee2fb3add80d3329aed461290310c85b42c8eda
Benjamin <ben@thefoghill.com>
2022-08-11 14:41:38 -0400

    clear items

--- src/App.js
+++ src/App.js
@@ -19,6 +19,7 @@ function App() {
       //deal with edit
     } else {
       //show alert
+      showAlert(true,'success','item added to the list')
       const newItems = { id: new Date().getTime().toString(), title: name };
       setList([...list, newItems]);
       setName("");
@@ -27,6 +28,11 @@ function App() {
 
   const showAlert = (show=false,type='',msg='') => {
     setAlert({show,type,msg})
+  }
+
+  const clearList = () => {
+    showAlert(true,'danger','empty list')
+    setList([])
   }
 
   return (
@@ -51,7 +57,7 @@ function App() {
         {list.length > 0 && (
           <div className="grocery-container">
             <List items={list} />
-            <button className="clear-btn">clear items</button>
+            <button className="clear-btn" onClick={clearList}>clear items</button>
           </div>
         )}
       </section>
