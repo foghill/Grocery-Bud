0415c643ec1ca36e7ea0bf65223c7639fe003439
Benjamin <ben@thefoghill.com>
2022-08-11 14:09:38 -0400

    form setup

--- src/App.js
+++ src/App.js
@@ -1,9 +1,33 @@
-import React, { useState, useEffect } from 'react'
-import List from './List'
-import Alert from './Alert'
+import React, { useState, useEffect } from "react";
+import List from "./List";
+import Alert from "./Alert";
 
 function App() {
-  return <h2>grocery bud setup</h2>
+  const [name, setName] = useState("");
+  const [list, setList] = useState([]);
+  const [isEditing, setIsEditing] = useState(false);
+  const [editID, setEditID] = useState(null);
+  const [alert, setAlert] = useState({ show: false, msg: "", type: "" });
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    console.log('hello')
+  }
+
+  return (
+    <>
+      <section className="section-center">
+        <form className='grocery-form' onSubmit={handleSubmit}></form>
+        {alert.show && <Alert />}
+        <div className='grocery-container'>
+          <List />
+          <button className="clear-btn">
+            clear items
+          </button>
+        </div>
+      </section>
+    </>
+  );
 }
 
-export default App
+export default App;
